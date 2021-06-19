98eb55177a773f4de2db37f412122fff0e1c46f7
Gabriel <Gabriel@DESKTOP-BR5EK8R>
2021-06-19 03:09:06 -0300

    controller

--- src/main/java/controller/ControladoraAplicacaoVacina.java
+++ src/main/java/controller/ControladoraAplicacaoVacina.java
@@ -1,5 +1,23 @@
 package controller;
 
+import exception.AplicacaoException;
+import model.bo.AplicacaoVacinaBO;
+import model.vo.AplicacaoVacinaVO;
+
 public class ControladoraAplicacaoVacina {
 
+	
+	private AplicacaoVacinaBO bo = new AplicacaoVacinaBO();
+	
+	public AplicacaoVacinaVO cadastrar(AplicacaoVacinaVO aplicacaoVacina) throws AplicacaoException {
+		return bo.cadastrar(aplicacaoVacina);
+	}
+	
+	public String validarCampos(AplicacaoVacinaVO aplicacaoVacina) {
+		String mensagem = " ";
+		if(aplicacaoVacina.getDataAplicacao() == null) {
+			mensagem += "O campo data da aplicação deve ser preenchdido.";
+		}
+		return mensagem;
+	}
 }
