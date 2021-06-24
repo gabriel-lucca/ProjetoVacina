1d867cd14eaaacdfdb5af72d4b4a161ea4319b06
Johnatan <Johnatan@PikachuDoMal>
2021-06-23 23:29:15 -0300

    Corrigido podeVacinar, cadastrar; Adicionado altear, excluir e buscar por id; Excluído: consultarPorCpf(Pessoa já possui esta função);

--- src/main/java/model/bo/AplicacaoVacinaBO.java
+++ src/main/java/model/bo/AplicacaoVacinaBO.java
@@ -10,8 +10,7 @@ import model.dao.VacinaDAO;
 import model.vo.AplicacaoVacinaVO;
 import model.vo.PessoaVO;
 import model.vo.VacinaVO;
-
-import exception.AnalisarCamposVacinaException;
+import exception.exception_aplicacao_vacina.AnalisarSePodeAplicarException;
 
 public class AplicacaoVacinaBO {
 
@@ -24,67 +23,49 @@ public class AplicacaoVacinaBO {
 	VacinaDAO vDAO = new VacinaDAO();
 	AplicacaoVacinaDAO avDAO = new AplicacaoVacinaDAO();
 
-	public boolean podeVacinar(String cpf, int id) throws AnalisarCamposVacinaException {
+	public String podeVacinar(String cpf, int id) {
 		String mensagem = null;
-		boolean resposta = false;
-		
-		AplicacaoVacinaDAO avDAO = new AplicacaoVacinaDAO();
 		PessoaVO avVO = new PessoaVO();
-
 		PessoaVO p = pDAO.consultarPorCpf(cpf);
+		VacinaVO v = vDAO.consultarPorId(id);
 		ArrayList<AplicacaoVacinaVO> aplicacoes = avDAO.consultarAplicacoes(p);
-		VacinaVO v = vDAO.buscarPorId(id);
-
 		Integer dosesAplicadas = aplicacoes.size();
-		Period periodo = Period.between(LocalDate.now(), aplicacoes.get(dosesAplicadas).getDataAplicacao().plusDays(v.getIntervaloDoses()));
+		LocalDate dtProximaAplicacao = aplicacoes.get(dosesAplicadas).getDataAplicacao().plusDays(v.getIntervaloDoses());
+		int tempoRestante = dtProximaAplicacao.compareTo(dtProximaAplicacao);
 		// obter as aplicacoes da vacina em questao
 		//a) nao atingiu o limite de doses - olhar a quantidade de aplicacoes
 		//b) intervalo entre a ultima dose e HOJE > intervalo da vacina em questao -- olhar a ultima aplicacao
 		//pode receber nova aplicacao SE(e somente se)
 		if(aplicacoes.size() == v.getQuantidadeDoses()) {
-			mensagem += "Esta pessoa atingiu o limite de doses";
+			mensagem += "\nEsta pessoa atingiu o limite de doses.\n Foram aplicadas "+aplicacoes.size();
+		} else if(tempoRestante > 0) {
+			mensagem += "\nAinda não pode aplicar.\nTempo restante "+tempoRestante;
 		}
-		if(aplicacoes.size() < v.getQuantidadeDoses()) {
-			mensagem += "Esta pessoa aplicou "+dosesAplicadas+" doses.\nRestam "+(v.getQuantidadeDoses()-dosesAplicadas)+" doses.\nProxima dose: "+aplicacoes.get(dosesAplicadas).getDataAplicacao().plusDays(v.getIntervaloDoses());
-		}
-		if(periodo.getDays() != v.getIntervaloDoses()) {
-			mensagem += "Restam "+periodo.getDays()+" dias at� a pr�xima dose.";	
-		}
-		if(mensagem == null) {
-			resposta = true;
-		} else {
-			resposta = false;
-			throw new AnalisarCamposVacinaException(mensagem);
-		}
-		 return resposta;
-		}
-	
-	public AplicacaoVacinaVO cadastrar(AplicacaoVacinaVO aplicacaoVacinaVO) throws AnalisarCamposVacinaException {
-		String mensagem = "";
-		boolean resposta = false;
+		return mensagem;
+	}
+	public AplicacaoVacinaVO cadastrar(AplicacaoVacinaVO aplicacaoVacinaVO) throws AnalisarSePodeAplicarException {
+		VacinaVO vVO = aplicacaoVacinaVO.getVacina();
 		PessoaVO pVO = aplicacaoVacinaVO.getPessoa();
 		
-		VacinaVO vVO = aplicacaoVacinaVO.getVacina();
-
-		if(podeVacinar(pVO.getCpf(), vVO.getIdVacina())) {
-			AplicacaoVacinaDAO avDAO = new AplicacaoVacinaDAO();
-			avDAO.cadastrar(aplicacaoVacinaVO);
-		} else {
-			throw new AnalisarCamposVacinaException(mensagem);
+		String resultadoPodeVacinar=null;
+		if(resultadoPodeVacinar != null && !resultadoPodeVacinar.isEmpty()) {
+			throw new AnalisarSePodeAplicarException(resultadoPodeVacinar);
 		}
 		return aplicacaoVacinaVO;
 	}
-
-	public PessoaVO consultarPorCpf(String cpf) {
-		return pDAO.consultarPorCpf(cpf);
+	public boolean alterar(AplicacaoVacinaVO aplicacaoAlterada) {
+		return avDAO.alterar(aplicacaoAlterada);
 	}
-
+	public boolean excluir(Integer id) {
+		return avDAO.excluir(id);
+	}
 	public ArrayList<AplicacaoVacinaVO> consultarAplicacoes(PessoaVO p) {
 		return avDAO.consultarAplicacoes(p);
 
 	}
-
-	
+	public AplicacaoVacinaVO consultarPorId(Integer id) {
+		return avDAO.consultarPorId(id);
+	}
 }
 
 
