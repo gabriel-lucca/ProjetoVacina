985ff18f8c00d2f16677744cc8e3d0cc4b441395
Johnatan <Johnatan@PikachuDoMal>
2021-06-28 10:26:24 -0300

    Script atualizado

--- script/ScriptProjetoVacina.sql
+++ script/ScriptProjetoVacina.sql
@@ -27,13 +27,14 @@ create table aplicacaoVacina(
 	idAplicacao int not null auto_increment primary key,
     idPessoa int not null,
     idVacina int not null,
-    dtAplicacao date not null
+    dtAplicacao date not null,
+	foreign key(IdPessoa) references pessoa(idPessoa) 
 );
 -- Inserts:
-insert into pessoa(nomePessoa, cpf, email, telefone, dtNascimento, cidade, estado, endereco) values("Bigodin Finin", "111.111.111-11", "Dr.BigodinFinin@gmail.com", "21911111111", '1970-06-04', "Rio de Janeiro", "Rio de Janeiro", "Logo alí"); 
-insert into vacina(nomePesquisadorResponsavel, paisOrigem, nomeVacina, dtInicioPesquisa, quantidadeDoses, intervaloDoses) values("Bigodin Finin", "Brasil", "CuraBr", '2020-05-05', 1, 0);
+insert into pessoa(nomePessoa, cpf, email, telefone, dtNascimento, cidade, estado, endereco) values("Gabriel", "111.111.111-11", "gabriel123@gmail.com", "21911111111", '1970-06-04', "Florianopolis", "SC", "Servidão Araquari"); 
+insert into vacina(nomePesquisadorResponsavel, paisOrigem, nomeVacina, dtInicioPesquisa, quantidadeDoses, intervaloDoses) values("Felipe Oliveira", "Brasil", "Coronavac", '2020-05-05', 2, 90);
 insert into aplicacaoVacina(idPessoa, idVacina, dtAplicacao) values(1, 1, '2021-03-02');
-insert into pessoa(nomePessoa, cpf, email, telefone, dtNascimento, cidade, estado, endereco) values("Usuario para teste de exclusão", "222.111.111-11", "Mr.BigodinGrossin@gmail.com", "21911111211", '1970-07-04', "Rio de Fevereiro", "Rio de JFevereiro", "Bem alí"); 
+insert into pessoa(nomePessoa, cpf, email, telefone, dtNascimento, cidade, estado, endereco) values("Usuario para teste de exclusão", "222.111.111-11", "BigodinGrossin@gmail.com", "21911111211", '1970-07-04', "Rio de Fevereiro", "Rio de JFevereiro", "Bem alí"); 
 -- Select:
 select * from pessoa;
 select * from vacina;
