d6772d3f8829ec01cabe582f444e87e53b373050
Gabriel <Gabriel@DESKTOP-BR5EK8R>
2021-06-28 23:08:24 -0300

    Script atualizado

--- script/ScriptProjetoVacina.sql
+++ script/ScriptProjetoVacina.sql
@@ -17,7 +17,7 @@ create table pessoa(
 create table vacina(
 	idVacina int not null auto_increment primary key,
     nomePesquisadorResponsavel varchar(100) not null,
-    paisorigem varchar(50) not null,
+    paisOrigem varchar(50) not null,
     nomeVacina varchar(50) not null,
     dtInicioPesquisa date not null,
     quantidadeDoses varchar(2) not null,
@@ -30,8 +30,7 @@ create table aplicacaoVacina(
     dtAplicacao date not null
 );
 -- Inserts:
-insert into pessoa(nomePessoa, cpf, email, telefone, dtNascimento, cidade, estado, endereco) values("Gabriel", "111.111.111-11", "gabriel123@gmail.com", "21911111111", '1970-06-04', "Florianopolis", "SC", "Servidão Araquari"); 
-insert into pessoa(nomePessoa, cpf, email, telefone, dtNascimento, cidade, estado, endereco) values("Usuario para teste de exclusão", "222.111.111-11", "BigodinGrossin@gmail.com", "21911111211", '1970-07-04', "Rio de Fevereiro", "Rio de JFevereiro", "Bem alí"); 
+insert into pessoa(nomePessoa, cpf, email, telefone, dtNascimento, cidade, estado, endereco) values("Gabriel Augusto", "111.111.111-11", "gabriel123@gmail.com", "21911111111", '1970-06-04', "Florianopolis", "SC", "Servidão Araquari"); 
 insert into pessoa(nomePessoa, cpf, email, telefone, dtNascimento, cidade, estado, endereco) values("André Jacinto", "245.046.289-11", "andrefilipe@gmail.com", "48911648911", '2000-08-08', "Florianopolis", "SC", "Rodovia Barreiros");
 insert into pessoa(nomePessoa, cpf, email, telefone, dtNascimento, cidade, estado, endereco) values("Gabriel Martins", "245.046.760-11", "gabrielmarti@gmail.com", "48911648333", '2001-08-01', "Florianopolis", "SC", "Rodovia Gonzaga");
 insert into pessoa(nomePessoa, cpf, email, telefone, dtNascimento, cidade, estado, endereco) values("Rodolfo bawbis", "215.489.289-11", "rodolfobawbis@gmail.com", "48911677911", '1980-05-08', "Florianopolis", "SC", "Avenida Ropi");
@@ -44,7 +43,7 @@ insert into pessoa(nomePessoa, cpf, email, telefone, dtNascimento, cidade, estad
 insert into vacina(nomePesquisadorResponsavel, paisOrigem, nomeVacina, dtInicioPesquisa, quantidadeDoses, intervaloDoses) values("Felipe Oliveira", "Brasil", "Coronavac", '2020-05-05', 2, 90);
 insert into vacina(nomePesquisadorResponsavel, paisOrigem, nomeVacina, dtInicioPesquisa, quantidadeDoses, intervaloDoses) values("VAC", "China", "Coronavac", '2020-06-05', 2, 90);
 insert into vacina(nomePesquisadorResponsavel, paisOrigem, nomeVacina, dtInicioPesquisa, quantidadeDoses, intervaloDoses) values("BioNTech", "Alemanha", "Pfizer", '2020-10-10', 2, 90);
-insert into vacina(nomePesquisadorResponsavel, paisOrigem, nomeVacina, dtInicioPesquisa, quantidadeDoses, intervaloDoses) values("Oxford", "Reino Unido", "Aztrazeneca", '2020-06-05', 2, 90);
+insert into vacina(nomePesquisadorResponsavel, paisOrigem, nomeVacina, dtInicioPesquisa, quantidadeDoses, intervaloDoses) values("Oxford", "Reino Unido", "Aztrazeneca", '2020-06-05', 3, 90);
 insert into vacina(nomePesquisadorResponsavel, paisOrigem, nomeVacina, dtInicioPesquisa, quantidadeDoses, intervaloDoses) values("Oxford", "índia", "Covaxin", '2020-11-11', 2, 90);
 
 insert into aplicacaoVacina(idPessoa, idVacina, dtAplicacao) values(1, 1, '2021-03-02');
