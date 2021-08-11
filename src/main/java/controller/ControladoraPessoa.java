cb8e14d8049b7ab4a902d62124ee71d0b3b02cd3
Gabriel <Gabriel@DESKTOP-BR5EK8R>
2021-08-11 19:21:58 -0300

    controller

--- src/main/java/controller/ControladoraPessoa.java
+++ src/main/java/controller/ControladoraPessoa.java
@@ -2,69 +2,83 @@ package controller;
 
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 import exception.exception_pessoa.AnalisarCamposPessoaException;
 import model.bo.PessoaBO;
 import model.dao.PessoaDAO;
 import model.vo.PessoaVO;
 import seletor.FiltroPessoa;
+import util.Data;
 
 public class ControladoraPessoa {
 
-private PessoaBO bo = new PessoaBO();
-private PessoaDAO dao = new PessoaDAO();
-	
+	private PessoaBO bo = new PessoaBO();
+	private PessoaDAO dao = new PessoaDAO();
+
 	public String cadastrar(PessoaVO novaPessoa) throws AnalisarCamposPessoaException {
 		String respostaValidacao = validarCampos(novaPessoa);
-		if(respostaValidacao != null) {
+		if (respostaValidacao != "") {
 			throw new AnalisarCamposPessoaException(respostaValidacao);
 		}
+
 		return bo.cadastrarPessoaBO(novaPessoa);
 	}
-	
+
 	public String validarCampos(PessoaVO pessoa) {
-		String mensagem=null;
-		//Os campos devem possuir no m�nimo d�gitos 3 e no m�ximo 100
-		//Nome:
-		if(pessoa.getNome().length() < 3) {
+		String mensagem = "";
+		// Os campos devem possuir no m�nimo d�gitos 3 e no m�ximo 100
+		// Nome:
+		if (pessoa.getNome().length() < 3) {
 			mensagem += "\nO nome deve conter no minimo 3 caracteres";
-		}else if(pessoa.getNome().length() > 100) {
+		} else if (pessoa.getNome().length() > 100) {
 			mensagem += "\nO nome nao pode passar de 100 caracteres";
 		}
-		//Email:
-		String email = pessoa.getEmail();		
+
+		// Email:
+		String email = pessoa.getEmail();
 		boolean respostaVerificacao = false;
-		for(int i = 0;i<email.length();i++) {
-			if(String.valueOf(email.charAt(i)).equalsIgnoreCase("@")){
+		for (int i = 0; i < email.length(); i++) {
+			if (String.valueOf(email.charAt(i)).equalsIgnoreCase("@")) {
 				respostaVerificacao = true;
 			}
-			}
-		if(pessoa.getEmail().length() < 3) {
-			mensagem += "\nO email deve conter no m�nimo 3 caracteres";
-		}else if(pessoa.getEmail().length() > 100) {
-			mensagem += "\nO email n�o pode passar de 100 caracteres";
-		}else if(!respostaVerificacao){
-			mensagem +="\nO email deve conter '@' ";
 		}
-		//Endereco:
-		if(pessoa.getEndereco().length() < 3) {
+		boolean respostaVerificacaoo = false;
+		for (int i = 0; i < email.length(); i++) {
+			if (String.valueOf(email.charAt(i)).equalsIgnoreCase(".")) {
+				respostaVerificacaoo = true;
+			}
+		}
+		if (pessoa.getEmail().length() < 3) {
+			mensagem += "\nO email deve conter no m�nimo 3 caracteres";
+		} else if (pessoa.getEmail().length() > 100) {
+			mensagem += "\nO email n�o pode passar de 100 caracteres";
+		} else if (!respostaVerificacao) {
+			mensagem += "\nO email deve conter '@' ";
+		} else if (!respostaVerificacaoo) {
+			mensagem += "\nO email deve conter '.' ";
+		}
+
+		// Endereco:
+		if (pessoa.getEndereco().length() < 3) {
 			mensagem += "\nO endereco deve conter no minimo 3 caracteres";
-		}else if(pessoa.getEndereco().length() > 100) {
+		} else if (pessoa.getEndereco().length() > 100) {
 			mensagem += "\nO endereco nao pode passar de 100 caracteres";
 		}
-		
-		//Os campos devem possuir no m�nimo d�gitos 3 e no m�ximo 50
-		//Cidade:
-		if(pessoa.getCidade().length() < 3) {
+
+		// Os campos devem possuir no m�nimo d�gitos 3 e no m�ximo 50
+		// Cidade:
+		if (pessoa.getCidade().length() < 3) {
 			mensagem += "\nA cidade deve conter no minimo 3 caracteres";
-		}else if(pessoa.getCidade().length() > 50) {
+		} else if (pessoa.getCidade().length() > 50) {
 			mensagem += "\nA cidade nao pode passar de 50 caracteres";
-		}		
+		}
 		return mensagem;
 	}
 
-	public boolean alterar(PessoaVO pessoaAlterada) throws AnalisarCamposPessoaException{
+	public boolean alterar(PessoaVO pessoaAlterada) throws AnalisarCamposPessoaException {
 		String respostaValidacao = validarCampos(pessoaAlterada);
-		if(respostaValidacao != null && !respostaValidacao.isEmpty()) {
+		if (respostaValidacao != null && !respostaValidacao.isEmpty()) {
 			throw new AnalisarCamposPessoaException(respostaValidacao);
 		}
 		return bo.alterar(pessoaAlterada);
@@ -75,24 +89,25 @@ private PessoaDAO dao = new PessoaDAO();
 	}
 
 	public PessoaVO consultarPorId(Integer idPessoaSelecionada) {
-		
+
 		return bo.consultarPorId(idPessoaSelecionada);
 	}
-	public ArrayList<PessoaVO> consultarTodos(){
+
+	public ArrayList<PessoaVO> consultarTodos() {
 		return bo.consultarTodos();
 	}
+
 	public boolean excluir(Integer id) {
 		return bo.excluir(id);
 	}
 
 	public ArrayList<PessoaVO> consultarComFiltro(FiltroPessoa seletor) {
 		return bo.consultarComFiltro(seletor);
-		
+
 	}
 
 	public ArrayList<PessoaVO> consultarTodosRelatorio() {
 		return bo.consultarTodosRelatorio();
 	}
-	
 
 }
--- src/main/java/controller/ControladoraVacina.java
+++ src/main/java/controller/ControladoraVacina.java
@@ -1,6 +1,5 @@
 package controller;
 
-
 import java.util.ArrayList;
 
 import exception.exceptionVacina.AnalisarCamposVacinaException;
@@ -11,68 +10,76 @@ import seletor.FiltroVacina;
 
 public class ControladoraVacina {
 	private VacinaBO bo = new VacinaBO();
-	
+
 	public String cadastrar(VacinaVO novaVacina) throws AnalisarCamposVacinaException, VacinaJaExisteException {
 		String respostaValidacao = validarCampos(novaVacina);
-		if(respostaValidacao != null) {
+		if (respostaValidacao != "") {
 			throw new AnalisarCamposVacinaException(respostaValidacao);
 		}
 		return bo.cadastrar(novaVacina);
 	}
+
 	public String validarCampos(VacinaVO vacina) {
-		String mensagem=null;
-		//Os campos devem possuir no m�nimo d�gitos 3 e no m�ximo 100
-		//NomePesquisador:
-		if(vacina.getNomePesquisadorResponsavel().length() < 3) {
+		String mensagem = "";
+		// Os campos devem possuir no m�nimo d�gitos 3 e no m�ximo 100
+		// NomePesquisador:
+		if (vacina.getNomePesquisadorResponsavel().length() < 3) {
 			mensagem += "\nO nome do pesquisador responsavel deve possuir no mínimo 3 caracteres";
-		}else if(vacina.getNomePesquisadorResponsavel().length() > 100) {
+		} else if (vacina.getNomePesquisadorResponsavel().length() > 100) {
 			mensagem += "\nO nome do pesquisador responsavel não deve possuir mais de 100 caracteres";
 		}
-		//paisOrigem:
-		//if(vacina.getPaisOrigem().length() < 3) {
-		//	mensagem += "\nO pais de origem deve possuir no mínimo 3 caracteres";
-		//}else if(vacina.getPaisOrigem().length() > 100) {
-		//	mensagem += "\nO pais de origem não deve possuir mais de 100 caracteres";
-		//}
-		
-		//NomeVacina:
-		if(vacina.getNomeVacina().length() < 3) {
+		// paisOrigem:
+		// if(vacina.getPaisOrigem().length() < 3) {
+		// mensagem += "\nO pais de origem deve possuir no mínimo 3 caracteres";
+		// }else if(vacina.getPaisOrigem().length() > 100) {
+		// mensagem += "\nO pais de origem não deve possuir mais de 100 caracteres";
+		// }
+
+		// NomeVacina:
+		if (vacina.getNomeVacina().length() < 3) {
 			mensagem += "\nO nome da vacina deve possuir no mínimo 3 caracteres";
-		}else if(vacina.getNomeVacina().length() > 50) {
+		} else if (vacina.getNomeVacina().length() > 50) {
 			mensagem += "\nO nome da vacina nao deve possuir mais de 50 caracteres";
 		}
-		
-		//Intervalo doses:
-		if(vacina.getIntervaloDoses() < 0) {
+
+		// Intervalo doses:
+		if (vacina.getIntervaloDoses() < 0) {
 			mensagem += "\nO Intervalo de doses deve possuir no mínimo 0 dias";
-		}else if(vacina.getIntervaloDoses() > 365) {
+		} else if (vacina.getIntervaloDoses() > 365) {
 			mensagem += "\nO Intervalo de doses tem um limite de 365 dias";
 		}
-		
+
 		return mensagem;
 	}
-	public boolean alterar(VacinaVO vacinaAlterada) throws AnalisarCamposVacinaException{
+
+	public boolean alterar(VacinaVO vacinaAlterada) throws AnalisarCamposVacinaException {
 		String respostaValidacao = validarCampos(vacinaAlterada);
-		if(respostaValidacao != null && !respostaValidacao.isEmpty()) {
+		if (respostaValidacao != null && !respostaValidacao.isEmpty()) {
 			throw new AnalisarCamposVacinaException(respostaValidacao);
 		}
 		return bo.alterar(vacinaAlterada);
 	}
-	public ArrayList<VacinaVO> consultarTodos(){
+
+	public ArrayList<VacinaVO> consultarTodos() {
 		return bo.consultarTodos();
 	}
+
 	public boolean excluir(Integer id) {
 		return bo.excluir(id);
 	}
+
 	public VacinaVO consultarPorNome(String nome) {
 		return bo.consultarPorNome(nome);
 	}
+
 	public VacinaVO consultarPorId(Integer id) {
 		return bo.consultarPorId(id);
 	}
+
 	public ArrayList<VacinaVO> consultarComFiltro(FiltroVacina seletor) {
 		return bo.consultarComFiltro(seletor);
 	}
+
 	public ArrayList<VacinaVO> consultarTodosRelatorio() {
 		return bo.consultarTodosRelatorio();
 	}
