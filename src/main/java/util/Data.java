2e72465fe85fdc02655eb7a7dfe1e48a2804b01e
Gabriel <Gabriel@DESKTOP-BR5EK8R>
2021-08-19 19:09:00 -0300

    Validacao data

--- src/main/java/util/Data.java
+++ src/main/java/util/Data.java
@@ -5,7 +5,7 @@ public class Data {
 	public String validarData(int dia, int mes, int ano) {
 		String mensagem = "" ;
 		
-		 if(dia>0 && dia<32 && mes>0 && mes<13 && ano>0 && ano<2022 &&
+		 if(dia>0 && dia<32 && mes>0 && mes<13 && ano>1900 && ano<2022 &&
 				 ((mes==1 || mes==3 || mes==5 || mes==7 || mes==8 || mes==10 || mes==12) || ((mes==4 || 
 				 mes==6 || mes==9 || mes==11) && dia<=30) || 
 				 (mes==2 &&(dia<=29 && ano%4==0 && (ano%100!=0 || ano%400==0))|| dia<=28))){
